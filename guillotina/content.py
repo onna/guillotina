d315271dc1d72ded43e375726027653c35dd7bb7
Jon Durbin <jon.durbin@onna.com>
2023-07-28 08:39:00 -0400

    More efficiencies.

--- guillotina/content.py
+++ guillotina/content.py
@@ -640,7 +640,7 @@ def get_all_behavior_interfaces(content) -> list:
     return behaviors
 
 
-async def get_all_behaviors(content, create=False, load=True) -> list:
+async def get_all_behaviors(content, create=False, load=True, preload_only=False) -> list:
     schemas = get_all_behavior_interfaces(content)
     instances = [schema(content) for schema in schemas]
 
@@ -656,6 +656,8 @@ async def get_all_behaviors(content, create=False, load=True) -> list:
             key = getattr(inst, "__annotations_data_key__", None)
             if key and key in annotation_data:
                 content.__gannotations__[key] = annotation_data[key]
+    if preload_only:
+        return
 
     # Load all (for all non-preload items, this takes care of initialization).
     behaviors = []
